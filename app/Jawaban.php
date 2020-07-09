b46e6c5e3c019b8845039288022e8374e5f4883d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-09 19:53:28 +0700

    model jawaban

--- app/Jawaban.php
+++ app/Jawaban.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jawaban extends Model
 {
-    //
+    protected $table = 'jawabans';
+
+    protected $fillable = [
+        'id_user', 'id_pertanyaan', 'isi', 'poin_vote'
+    ];
 }
