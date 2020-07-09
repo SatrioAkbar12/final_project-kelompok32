ecbe0cb86c616a31d706694e75aef024489a8362
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-10 01:36:08 +0700

    template view master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -324,7 +324,7 @@
   <script src="{{asset('/sbadmin2/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+  <script src="{{asset('/sbadmin2/js/sb-admin-2.min.js')}}"></script>
 
 </body>
 
