8dc2b2db2e2b3b1dd8bd53ae1cf1260119635a7c
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-09 19:53:16 +0700

    model pertanyaan

--- app/Pertanyaan.php
+++ app/Pertanyaan.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pertanyaan extends Model
 {
-    //
+    protected $table = 'pertanyaans';
+
+    protected $fillable = [
+        'id_user','judul', 'isi', 'id_jawabanTepat','poin_vote'
+    ];
 }
