ec31487284ff2125b5cb3895c9fc996167dd6ec6
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-10 01:36:45 +0700

    controller pertanyaan

--- app/Http/Controllers/PertanyaanController.php
+++ app/Http/Controllers/PertanyaanController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pertanyaan;
+use Illuminate\Support\Facades\Auth;
 
 class PertanyaanController extends Controller
 {
-    //
+    public function create(){
+        return view('pertanyaan.form_pertanyaan');
+    }
+
+    public function store(Request $request){
+        Pertanyaan::create([
+            'id_user' => Auth::id(),
+            'judul' => $request->judul,
+            'isi' => $request->isi,
+            'poin_vote' => 0
+        ]);
+
+        return redirect('/home');
+    }
 }
