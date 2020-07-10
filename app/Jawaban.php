2474b1d029dfad53867cbd93c66a352037a61d73
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-10 14:55:33 +0700

    model

--- app/Jawaban.php
+++ app/Jawaban.php
@@ -11,4 +11,28 @@ class Jawaban extends Model
     protected $fillable = [
         'id_user', 'id_pertanyaan', 'isi', 'poin_vote'
     ];
+
+    // public function user(){
+    //     return $this->belongsTo('App/User');
+    // }
+
+    // public function pertanyaan(){
+    //     return $this->belongsTo('App/Pertanyaan');
+    // }
+
+    // public function komentar(){
+    //     return $this->hasMany('App/Komentar');
+    // }
+
+    public function user(){
+        return $this->belongsTo('User');
+    }
+
+    public function pertanyaan(){
+        return $this->belongsTo('Pertanyaan');
+    }
+
+    public function komentar(){
+        return $this->hasMany('Komentar');
+    }
 }
--- app/Pertanyaan.php
+++ app/Pertanyaan.php
@@ -9,7 +9,7 @@ class Pertanyaan extends Model
     protected $table = 'pertanyaans';
 
     protected $fillable = [
-        'id_user','judul', 'isi', 'id_jawabanTepat','poin_vote'
+        'id_user','judul', 'isi', 'tag', 'id_jawabanTepat','poin_vote'
     ];
     /*
     stuck bingung join pake eloquent...
@@ -20,4 +20,28 @@ class Pertanyaan extends Model
     public function user(){
         return $this->belongsTo('App\User', 'id_user', 'id');
     }*/
+
+    // public function user(){
+    //     return $this->belongsTo('App/User','id_user', 'id');
+    // }
+
+    // public function jawaban(){
+    //     return $this->hasMany('App/Jawaban');
+    // }
+
+    // public function komentar(){
+    //     return $this->hasMany('App/Komentar');
+    // }
+
+    public function user(){
+        return $this->belongsTo('App/User');
+    }
+
+    public function jawaban(){
+        return $this->hasMany('App/Jawaban');
+    }
+
+    public function komentar(){
+        return $this->hasMany('App/Komentar');
+    }
 }
