98ff5da832dc2f17648022819a7a995d36070a54
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-10 14:54:58 +0700

    view pertanyaan+jawaban

--- resources/views/pertanyaan/show_pertanyaan.blade.php
+++ resources/views/pertanyaan/show_pertanyaan.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.master')
 
-@section('head_title', '{{$pertanyaan->judul}}')
+@section('head_title')
+    {{$pertanyaan->judul}}
+@endsection
+
+@push('script_head')
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+@endpush
 
 @section('konten')
-    <h2 class="mb-4">{{$pertanyaan->judul}}</h2>
+    {{-- <h2 class="mb-4">{{$pertanyaan->judul}}</h2>
 
     <div class="card shadow mb-4">
         <div class="card-body">
-                <p>{{$pertanyaan->isi}}</p>
+                <p>{!! $pertanyaan->isi !!}</p>
 
         </div>
     </div>
@@ -34,5 +40,99 @@
 
             </form>
         </div>
+    </div> --}}
+
+    <h2 class="mb-4">{{$pertanyaan->judul}}</h2>
+    <p class="ml-4"><strong>Dibuat pada {{$pertanyaan->created_at}}, oleh {{$user_pertanyaan->name}}</strong></p>
+
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            {!! $pertanyaan->isi !!}
+            <p>Tag :<br>
+                @foreach ($tag as $t)
+                    <button class="btn btn-info">{{$t}}</button>
+                @endforeach
+            </p>
+        </div>
     </div>
+
+    <h3 class="mb-3">Jawaban</h3>
+
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            @if(count($jawaban) == 0)
+                <h6>Masih belum ada jawaban nih!</h6>
+            @else
+                @for($i=0; $i<count($jawaban); $i++)
+                    <div class="card border-left-success mb-3">
+                        <div class="card-header">
+                            {{ $user_jawaban[$i] }}
+                        </div>
+                        <div class="card-body">
+                            {!! $jawaban[$i]->isi !!}
+                            <div class="text-right">
+                                {{ $jawaban[$i]->created_at }}
+                            </div>
+                        </div>
+                    </div>
+
+                @endfor
+            @endif
+        </div>
+    </div>
+
+    <div class="card shadow mb-4">
+        <div class="card-header bg-info">
+            <h5 class="text-light">Buat Jawabanmu Sendiri!</h5>
+        </div>
+        <div class="card-body">
+            <form action="/pertanyaan/{{$pertanyaan->id}}" method="POST">
+                @csrf
+
+                <textarea name="isi" class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
+
+                <button type="submit" class="mt-2 btn btn-primary">Jawab!</button>
+            </form>
+        </div>
+    </div>
+
 @endsection
+
+@push('script_body')
+  <script>
+    var editor_config = {
+      path_absolute : "/",
+      selector: "textarea.my-editor",
+      plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+      ],
+      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+      relative_urls: false,
+      file_browser_callback : function(field_name, url, type, win) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+        if (type == 'image') {
+          cmsURL = cmsURL + "&type=Images";
+        } else {
+          cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.open({
+          file : cmsURL,
+          title : 'Filemanager',
+          width : x * 0.8,
+          height : y * 0.8,
+          resizable : "yes",
+          close_previous : "no"
+        });
+      }
+    };
+
+    tinymce.init(editor_config);
+  </script>
+@endpush
